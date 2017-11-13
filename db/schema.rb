d6086139510a2d7a86d13bfa8ca5e805e931d5dc
Cristiane Silva <cristiane.hdo@gmail.com>
2017-11-13 15:19:10 +0000

    created booking and pet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113145413) do
+ActiveRecord::Schema.define(version: 20171113151818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "pet_id"
+    t.bigint "user_id"
+    t.string "category"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pet_id"], name: "index_bookings_on_pet_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.string "size"
+    t.string "breed"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,4 +55,7 @@ ActiveRecord::Schema.define(version: 20171113145413) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "pets"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "pets", "users"
 end
