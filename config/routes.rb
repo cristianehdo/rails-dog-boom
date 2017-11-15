adb6e17c02c6f5fc384729f68fbbf5acef0ce9b4
Cristiane Silva <cristiane.hdo@gmail.com>
2017-11-15 11:07:59 +0000

    index is now the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'services#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :services, only: [:index, :new, :create, :show, :edit, :update] do
     collection do
