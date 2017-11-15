932a7536cc5a3c715ed3c5b4b6c85e12897d6607
Cristiane Silva <cristiane.hdo@gmail.com>
2017-11-15 15:22:31 +0000

    updating working

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -33,8 +33,8 @@ class ServicesController < ApplicationController
   end
 
   def update
-    @service.update(service_params)
-    if @service.save
+    authorize(@service)
+    if @service.update(service_params)
       redirect_to service_path(@service)
     else
       render "edit"
@@ -42,7 +42,6 @@ class ServicesController < ApplicationController
   end
 
   def destroy
-     @service = find_service
      @service.destroy
      authorize(@service)
      redirect_to services_path(@service)
