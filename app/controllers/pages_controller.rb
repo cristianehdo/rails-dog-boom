f43dfeae2a3841be75859fe70a8c1165229aa14f
Nathalia de Luccas Filopoulos <nathiluccas@gmail.com>
2017-11-17 10:29:52 +0000

    dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
-  def dashboard
-   current_user
-  end
+  # def dashboard
+
+  # end
+
+  # def dashboard_params
+  #   params.require(:users).permit(:name)
+  # end
 end
