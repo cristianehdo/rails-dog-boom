0e0d0a96367c7ee8ea4002edf1806b74400ef6fe
Nathalia de Luccas Filopoulos <nathiluccas@gmail.com>
2017-11-17 17:17:02 +0000

    FUCCCKK

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -22,3 +22,18 @@ $("#model-close").click(function(){
   $('body').css('overflow', 'scroll');
 });
 
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#img-preview').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#img-file").change(function(){
+        readURL(this);
+    });
