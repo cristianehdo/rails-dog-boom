7d6bf62d56aa2b177c361b6a829a95aa028838e1
David Rossiter <david_rossiter@hotmail.com>
2017-11-17 13:05:32 +0000

    friday stuff

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -42,19 +42,30 @@ $("#menu-trigger").click(function(){
     $("#nav-menu").fadeToggle();
 });
 
-
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                $('#img-preview').attr('src', e.target.result);
-            }
-
-            reader.readAsDataURL(input.files[0]);
-        }
+//image preview
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $('#img-preview').attr('src', e.target.result);
     }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+$("#img-file").change(function(){
+  readURL(this);
+});
 
-    $("#img-file").change(function(){
-        readURL(this);
-    });
+//model open
+$("#open-create").click(function(){
+  var scrollTop = window.scrollY;
+  $('#model-cover').css('top', scrollTop + "px");
+  $('#model-cover').fadeToggle();
+  $('body').css('overflow', 'hidden');
+});
+
+$("#model-close").click(function(){
+  $('#model-cover').fadeToggle();
+  $('body').css('overflow', 'scroll');
+});
+
